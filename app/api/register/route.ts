17ba198a14a6662d1abcd418e655671b12cc9318
Pichborith Kong <pichborith@gmail.com>
2023-06-02 14:57:03 -0700

    connect prima to mongodb and add next-auth

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,17 +1,11 @@
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 
-import prisma from "@/app/libs/prismadb";
-import { NextResponse } from "next/server";
+import prisma from '@/app/libs/prismadb';
+import { NextResponse } from 'next/server';
 
-export async function POST(
-  request: Request
-) {
+export async function POST(request: Request) {
   const body = await request.json();
-  const {
-    email,
-    name,
-    password
-  } = body;
+  const { email, name, password } = body;
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -19,8 +13,8 @@ export async function POST(
     data: {
       email,
       name,
-      hashedPassword
-    }
+      hashedPassword,
+    },
   });
 
   return NextResponse.json(user);
